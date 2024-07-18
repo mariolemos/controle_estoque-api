46b37861dbb685e1de712eb44fa78998623252d7
lemos <lemos>
2024-07-18 16:12:26 -0300

    Projeto Criado

---
+++ src/main/java/br/com/mariolemos/Controle_Estoque_Api/ControleEstoqueApiApplication.java
@@ -0,0 +1,14 @@
+package br.com.mariolemos.Controle_Estoque_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleEstoqueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleEstoqueApiApplication.class, args);
+		System.out.println("Projeto Criado");
+	}
+
+}
